95160e40803fee741ee97d0b1e74a18622e210f1
Sergey Davlitshin <sergey.davlitshin@icloud.com>
2025-04-23 23:07:28 +0200

    Initial commit with Xcode project

---
+++ BBQTimerTests/BBQTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BBQTimerTests.swift
+//  BBQTimerTests
+//
+//  Created by Sergey Davlitshin on 23.04.2025.
+//
+
+import Testing
+@testable import BBQTimer
+
+struct BBQTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BBQTimerUITests/BBQTimerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BBQTimerUITestsLaunchTests.swift
+//  BBQTimerUITests
+//
+//  Created by Sergey Davlitshin on 23.04.2025.
+//
+
+import XCTest
+
+final class BBQTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
